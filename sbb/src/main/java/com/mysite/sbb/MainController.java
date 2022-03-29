c9793f300a9618ba99d2c7c3a37a7c12dab2e4a9
pahkey <pahkey@gmail.com>
2022-03-29 13:36:00 +0900

    2-02

---
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,15 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+}
