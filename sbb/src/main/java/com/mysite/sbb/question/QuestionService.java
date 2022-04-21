4a3b35dbb9ad43ffad29f957b0f48f619f56c4af
pahkey <pahkey@gmail.com>
2022-04-21 18:02:31 +0900

    3-01

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,26 +1,17 @@
 package com.mysite.sbb.question;
 
-import java.util.List;
-
-import org.modelmapper.ModelMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
-    private final ModelMapper modelMapper;
-    
-    private QuestionDto of(Question question) {
-        return modelMapper.map(question, QuestionDto.class);
-    }
-    
-    public List<QuestionDto> getList() {
-        List<Question> questionList = this.questionRepository.findAll();
-        List<QuestionDto> questionDtoList = questionList.stream().map(q -> of(q)).toList();
-        return questionDtoList;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
     }
 }
