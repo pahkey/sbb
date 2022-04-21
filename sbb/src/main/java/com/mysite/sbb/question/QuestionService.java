8250d104cb4dc6caf205a62a79107a45245d7a8e
pahkey <pahkey@gmail.com>
2022-04-21 18:36:05 +0900

    3-01

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,49 +1,37 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Service;
-
-import com.mysite.sbb.DataNotFoundException;
-
-import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
-    private final ModelMapper modelMapper;
-    
-    private QuestionDto of(Question question) {
-        return modelMapper.map(question, QuestionDto.class);
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
     }
     
-    public List<QuestionDto> getList() {
-        List<Question> questionList = this.questionRepository.findAll();
-        List<QuestionDto> questionDtoList = questionList.stream().map(q -> of(q)).collect(Collectors.toList());
-        return questionDtoList;
-    }
-    
-    public QuestionDto getQuestion(Integer id) {  
+    public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
         if (question.isPresent()) {
-            return of(question.get());
+            return question.get();
         } else {
             throw new DataNotFoundException("question not found");
         }
     }
     
-    public QuestionDto create(String subject, String content) {
+    public void create(String subject, String content) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
-        q = this.questionRepository.save(q);
-        return of(q);
+        this.questionRepository.save(q);
     }
 }
